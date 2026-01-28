4022bc855392a1582a875addf2beebf2e33aae4e
Nam Pho <npho@uw.edu>
2026-01-28 13:02:27 -0800

    Creating a submission script to pack up relevant files for assignment 1.

---
+++ assignment1/submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+assignment1=(
+	# Section 1
+    "silu/silu_torch.py"
+	"silu/silu_triton_kernel.py"
+	"silu/silu_triton_test.py"
+	"silu/torch_silu.json"
+	"silu/torch_silu.nsys-rep"
+	"silu/CUDA/silu.cu"
+	"silu/CUDA/main.cu"
+	"silu/CUDA/cuda_silu.ncu-rep"
+
+	# Section 2
+	"rms_norm/matrix/rms_norm_matrix.cu"
+	"rms_norm/matrix/main.cu"
+	"profiling/rms_norm_matrix.ncu-rep"
+	"rms_norm/vector/rms_norm_vector.cu"
+	"rms_norm/vector/main.cu"
+	"profiling/rms_norm_vector.ncu-rep"
+
+	# Section 3
+	"host_GPU/main.cu"
+	#"host_GPU/copy_first_column.cu" # TODO
+	#"host_GPU/copy.ncu-rep" # TODO
+
+	# Report
+	#"hw1-group14.pdf"
+)
+
+echo "Packing up assignment 1..."
+
+# The -r flag makes it recursive (useful for directories)
+zip -r assignment1-group14.zip "${assignment1[@]}"
