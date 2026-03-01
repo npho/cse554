86013c91170e3c3a0a5a838ade6a608e64868ccb
Nam Pho <npho@uw.edu>
2026-03-01 13:09:30 -0800

    End-to-end profiling script for benchmarking and plotting.

---
+++ assignment3/Section1/profile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# profile.sh
+# CSE 554 Group 14
+# Runs all the scripts at once and plots the results.
+
+echo "=== Running CUBLAS GEMM profiling ==="
+./run1_cublas.sh
+
+echo "=== Running CUTLASS GEMM profiling ==="
+./run2_cutlass.sh
+
+echo "=== Plotting results ==="
+python3 plot_gemm.py
